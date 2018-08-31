a88b141ecec42973d6ff893c66d22cfed0b388aa
André Bortolli <39111068+andrebortolli@users.noreply.github.com>
2018-08-31 02:22:40 -0300

    Protótipo de jogo
    Usar controle para controlar o player 1

---
+++ Assets/Scripts/Jogador.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts
+{
+    //Criar classe AI e herdar para utilizar o isAI.
+    class Jogador
+    {
+        private int score;
+        public int Score
+        {
+            get
+            {
+                return this.score;
+            }
+            set
+            {
+                this.score = value;
+            }
+        }
+        private bool isAI;
+        public bool IsAI
+        {
+            get
+            {
+                return this.isAI;
+
+            }
+            set
+            {
+                this.isAI = value;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player1Controlador.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Controlador : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float multiplicadorDeVelocidade = 35f;
+    public Vector2 controleXY;
+	// Use this for initialization
+	void Awake ()
+    {
+        rb = GetComponent<Rigidbody>();	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        controleXY = new Vector2(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"));
+        rb.AddForce(-controleXY.x * multiplicadorDeVelocidade, 0.0f, controleXY.y * multiplicadorDeVelocidade);
+        //transform.Translate(-controleXY.x * Time.deltaTime * 2.5f, 0.0f, controleXY.y * Time.deltaTime * 2.5f);
+	}
+}
