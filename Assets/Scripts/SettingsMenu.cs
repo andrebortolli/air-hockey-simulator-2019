b4653669b192c4855a7748dbe98668122293c10b
chaotichawk <gabrielgdev@gmail.com>
2018-10-01 16:37:41 -0300

    Base pro Settings/Mixer

---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer mainMixer;
+
+    //Lembrar de Salvar/Dar Load no playerprefs depois de mudar settings
+    public void SetVolumeMaster(float volume)
+    {
+        mainMixer.SetFloat("masterVolume", volume);
+    }
+
+    public void SetVolumeMusic(float volume)
+    {
+        mainMixer.SetFloat("musicVolume", volume);
+    }
+
+    public void SetVolumeSfx(float volume)
+    {
+        mainMixer.SetFloat("sfxVolume", volume);
+    }
+}
