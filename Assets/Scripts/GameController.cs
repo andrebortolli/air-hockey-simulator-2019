05a9f966abfe6825a077b92ebdceae9277e98f38
André Bortolli <39111068+andrebortolli@users.noreply.github.com>
2018-10-15 23:01:19 -0300

    Added PauseGame method.
    Still have some bugs to fix.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,12 +7,85 @@ using Scripts.DebugInfo;
 
 public class GameController : MonoBehaviour
 {
+    private bool isMainMenuVisible;
+    public bool IsMainMenuVisible
+    {
+        get
+        {
+            return isMainMenuVisible;
+        }
+        set
+        {
+            isMainMenuVisible = value;
+        }
+    }
+    private bool isPaused;
+    public bool IsPaused
+    {
+        get
+        {
+            return isPaused;
+        }
+    }
+    public List<GameObject> gameObjectsToFreezeOnPause;
+    public List<GameObject> gameObjectsToDisplayOnPause;
+    public Vector3[] frozenGameObjectsVelocities;
     private int[] playerScore = new int[2];
     public bool enableDebug;
     private DebugInformation dbgInfo;
     public TMP_Text fpsCounter;
     public TMP_Text frameCounter;
     public List<PlayerController> players;
+
+    public void DemoMode(bool value)
+    {
+        //Demo Mode code here.
+    }
+
+    public void PauseGame(bool pause)
+    {
+        isPaused = pause;
+        if (gameObjectsToFreezeOnPause != null)
+        {
+            if (!isPaused)
+            {
+                for (int i = 0; i < gameObjectsToFreezeOnPause.Count; i++)
+                {
+                    gameObjectsToFreezeOnPause[i].GetComponent<MonoBehaviour>().enabled = true;
+                    Rigidbody rb = gameObjectsToFreezeOnPause[i].GetComponent<Rigidbody>();
+                    if (rb && frozenGameObjectsVelocities != null)
+                    {
+                        rb.isKinematic = false;
+                        rb.detectCollisions = true;
+                        rb.velocity = frozenGameObjectsVelocities[i];
+                    }
+                }
+                foreach (GameObject go in gameObjectsToDisplayOnPause)
+                {
+                    go.SetActive(true);
+                }
+            }
+            else
+            {
+                frozenGameObjectsVelocities = new Vector3[gameObjectsToFreezeOnPause.Count];
+                for (int i = 0; i < gameObjectsToFreezeOnPause.Count; i++)
+                {
+                    gameObjectsToFreezeOnPause[i].GetComponent<MonoBehaviour>().enabled = false;
+                    Rigidbody rb = gameObjectsToFreezeOnPause[i].GetComponent<Rigidbody>();
+                    if (rb)
+                    {
+                        frozenGameObjectsVelocities[i] = rb.velocity;
+                        rb.isKinematic = true;
+                        rb.detectCollisions = false;
+                    }
+                }
+                foreach (GameObject go in gameObjectsToDisplayOnPause)
+                {
+                    go.SetActive(false);
+                }
+            }
+        }
+    }
 
     private void UpdateDebugInformationUI()
     {
