d31c2ea8c6f5a4e5f530575b44eb116dfc2a103d
André Bortolli <39111068+andrebortolli@users.noreply.github.com>
2018-11-23 17:11:52 -0200

    Removed Demo Mode; Added easter egg; Fixed Result Screen;

--- Assets/Scripts/ResultsScreen.cs
+++ Assets/Scripts/ResultsScreen.cs
@@ -5,8 +5,9 @@ using TMPro;
 
 public class ResultsScreen : MonoBehaviour
 {
-    public TMP_Text player1Score, player2Score, results;
+    public TMP_Text player1 ,player2, player1Score, player2Score, results;
     private GameController gameController;
+    Color32 player1Color = new Color32(102,64,255,255), player2Color = new Color32(255, 119, 91,255);
 	// Use this for initialization
 	void Awake ()
     {
@@ -15,16 +16,34 @@ public class ResultsScreen : MonoBehaviour
 
     private void OnEnable()
     {
+        if (gameController.players[0].aI)
+        {
+            player1.text = "AI 1";
+        }
+        else
+        {
+            player1.text = "Player 1";
+        }
+        if (gameController.players[1].aI)
+        {
+            player2.text = "AI 2";
+        }
+        else
+        {
+            player2.text = "Player 2";
+        }
         player1Score.text = string.Format("{0:D2}", gameController.players[0].GetPlayerScore());
         player2Score.text = string.Format("{0:D2}", gameController.players[1].GetPlayerScore());
         if (gameController.players[0].GetPlayerScore() > gameController.players[1].GetPlayerScore())
         {
             if (gameController.players[0].aI)
             {
+                results.GetComponent<TextMeshProUGUI>().faceColor = player1Color;
                 results.text = "AI 1 Won!";
             }
             else
             {
+                results.GetComponent<TextMeshProUGUI>().faceColor = player1Color;
                 results.text = "Player 1 Won!";
             }
         }
@@ -32,15 +51,18 @@ public class ResultsScreen : MonoBehaviour
         {
             if (gameController.players[1].aI)
             {
+                results.GetComponent<TextMeshProUGUI>().faceColor = player2Color;
                 results.text = "AI 2 Won!";
             }
             else
             {
+                results.GetComponent<TextMeshProUGUI>().faceColor = player2Color;
                 results.text = "Player 2 Won!";
             }
         }
         else if (gameController.players[0].GetPlayerScore() == gameController.players[1].GetPlayerScore())
         {
+            results.GetComponent<TextMeshProUGUI>().faceColor = Color.white;
             results.text = "Draw!";
         }
         else
---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StartMenu : MonoBehaviour {
+
+    public bool easterEggActivated = false;
+    public TMP_Dropdown dropdown;
+    // Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.Space) && Input.GetKey(KeyCode.E) && Input.GetKeyDown(KeyCode.D) && easterEggActivated == false)
+        {
+            TMP_Dropdown.OptionData demoModeOption = new TMP_Dropdown.OptionData("Demo Mode");
+            dropdown.options.Add(demoModeOption);
+            easterEggActivated = true;
+        }
+    }
+}
